8c7930e5b4e21074d0efcd551ae5a854aeedc49b
tanguy deleage <tanguy.deleage0@gmail.com>
2022-04-26 11:59:46 +0200

    Tentative de js, structure terminée

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const hamburger = document.querySelector('.navbar');
-const navLink = document.querySelector('.hzmburger-menu');
-
-hamburger.addEventListener('click', () => {
-  navLink.classList.toggle('hide');
-});
+function scrollHeader() {
+  const link = document.querySelectorAll('nav_links')
+  // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 80) link.classList.add('.here'); else link.classList.remove('.here')
+}
+window.addEventListener('scroll', scrollHeader)
